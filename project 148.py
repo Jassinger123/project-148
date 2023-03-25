d019238cce814d053d5ae2ed68221b9e70ce887e
Jassinger123 <104770712+Jassinger123@users.noreply.github.com>
2023-03-25 17:00:44 +0530

    Add files via upload

---
+++ project 148.py
@@ -0,0 +1,25 @@
+from tkinter import *
+import random 
+
+root=Tk()
+root.title("List")
+root.geometry("400x400")
+
+random_number_list = Label(root)
+random_number_sorted_list = Label(root)
+
+def randomlist():
+    randomlist = random.sample(range(20,40),10)
+    random_number_list["text"] = "random list : " + str(randomlist)
+    randomlist.sort()
+    random_number_sorted_list["text"] = "sorted random numbers : " + str(randomlist)
+    
+btn=Button(root,text="generate random lust ",command=randomlist)
+btn.place(relx=0.5,rely=0.4,anchor=CENTER)
+
+random_number_list.place(relx=0.5,rely=0.5,anchor=CENTER)    
+random_number_sorted_list.place(relx=0.5,rely=0.6,anchor=CENTER)
+
+root.mainloop()
+
+
